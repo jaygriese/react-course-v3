8c9a69b5b6e2eafdec1516836e83e326341bdbc9
James Griesedieck <jamesgriesedieck@Jamess-MacBook.local>
2023-07-13 10:53:19 -0500

    p5

---
+++ 04-fundamental-projects/05-menu/starter/src/Menu.jsx
@@ -0,0 +1,10 @@
+import MenuItems from "./MenuItems";
+
+const Menu = ({ items }) => {
+  return <div className="section-center">
+    {items.map((menuItems) => {
+        return <MenuItems key={menuItems.id} {...menuItems}/>
+    })}
+  </div>;
+};
+export default Menu;
